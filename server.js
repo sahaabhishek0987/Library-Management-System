1e3484cde90903c842e0bc713ac098b477785b4a
sahaabhishek0987 <saha.abhishek0987@gmail.com>
2026-05-26 17:54:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
 // Serve static frontend files
 app.use(express.static(path.join(__dirname, "public")));
 
