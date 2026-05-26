66e08c161a8b5a6c2ce36dbf0607ba95a4b75070
sahaabhishek0987 <saha.abhishek0987@gmail.com>
2026-05-26 19:20:47 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -17,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters"],
-      select: false, // never return password by default
+      select: false,
     },
     fullName: {
       type: String,
@@ -74,8 +73,6 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-
-// Auto-generate employee ID
 userSchema.pre("save", async function (next) {
   if (!this.employeeId) {
     const count = await mongoose.model("User").countDocuments();
@@ -84,26 +81,18 @@ userSchema.pre("save", async function (next) {
   }
   next();
 });
-
-// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
-
-// Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
-
-// Check if account is locked
 userSchema.methods.isLocked = function () {
   return this.lockedUntil && this.lockedUntil > Date.now();
 };
-
-// Default issuer permissions
 userSchema.pre("save", function (next) {
   if (this.isNew && this.role === "admin") {
     this.permissions = {
@@ -116,5 +105,4 @@ userSchema.pre("save", function (next) {
   }
   next();
 });
-
 module.exports = mongoose.model("User", userSchema);
